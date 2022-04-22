3c6fb52bceb408ed7505f9eb64e1b25a4758b41d
dhktpm16ftthk22022@gmail.com <98084587+Meivh@users.noreply.github.com>
2022-04-22 22:45:17 +0700

    update

---
+++ js/MainPageJS.js
@@ -0,0 +1,19 @@
+function openFormLogin() {
+    document.getElementById("formDangNhap").style.display = "block";
+}
+  
+function closeFormLogin() {
+    document.getElementById("formDangNhap").style.display = "none";
+}
+
+function openFormSignUp() {
+    document.getElementById("formDangKi").style.display = "block";
+}
+  
+function closeFormSignUp() {
+    document.getElementById("formDangKi").style.display = "none";
+}
+
+function scrollLoa() {
+    window.scrollTo(0, 500)
+}
